a0e12d3c7dc3fa9f03c1dac8b79e613a21b11444
Xevion <xevioni@yandex.com>
2020-08-17 02:32:55 -0500

    apply all previous adjustments to spirit text

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
                     Sojourner & Spirit
                 </h3>
             </b-navbar-brand>
-            <b-navbar-nav v-b-scrollspy:content>
+            <b-navbar-nav>
                 <b-nav-item link-classes="open-sans text-white font-weight-bold p-0" class="d-flex" v-if="showBreakpoint">
                     <b-badge variant="dark">
                         <span id="marker-xs" class="d-sm-none">XS</span>
@@ -22,7 +22,7 @@
             <b-jumbotron fluid container-fluid="fluid" class="text-dark py-3 px-md-5 px-sm-3">
                 <b-row class="px-sm-3 px-md-3 w-100 justify-content-center justify-content-lg-between">
                     <b-col class="d-block d-lg-none py-2 pb-3 py-lg-0" cols="0" sm="7" lg="0" align-self="center">
-                        <b-img fluid-grow src="./assets/sojourner.jpg"></b-img>
+                        <b-img fluid-grow class="shadow-lg" src="./assets/sojourner.jpg"></b-img>
                     </b-col>
                     <b-col cols="12" lg="8" xl="7" align-self="center">
                         <h1 id="sojourner">
@@ -44,6 +44,96 @@
                     </b-col>
                     <b-col class="d-none d-lg-block" cols="0" lg="4" xl="3" align-self="center">
                         <b-img fluid-grow src="./assets/sojourner.jpg"></b-img>
+                    </b-col>
+                </b-row>
+            </b-jumbotron>
+
+            <b-jumbotron fluid container-fluid="fluid px-0" class="bg-black py-md-5 py-0 px-2 px-md-2 px-lg-3">
+                <b-row class="px-3 pl-4 px-md-4 w-100 justify-content-center justify-content-lg-around">
+                    <b-col cols="12" lg="6" align-self="top">
+                        <h3>Communications</h3>
+                        <p class="ml-4 ml-sm-3">
+                            Sojourner is unique among the 4 rovers to successfully deploy on Mars in that it required
+                            it's lander to stay in communication with Earth. The rover was not equipped with a high gain
+                            antenna, which is required in order to communicate with Earth over large distances.
+                        </p>
+                        <p class="ml-4 ml-sm-3">
+                            This limitation forced the rover to stay within a certain range of the lander, and
+                            throughout it's lifetime on Mars, it never traveled more than a few dozen feet from the
+                            lander.
+                        </p>
+                        <h3>Tools</h3>
+                        <p class="ml-4 ml-sm-3">
+                            Sojourner was designed to study the surface of Mars by collecting and analyzing rock
+                            samples, taking pictures and more using the following tools:
+                        </p>
+                        <ul class="ml-3 ml-lg-4">
+                            <li class="py-1 pt-2">
+                                An <abbr title="Alpha Proton X-ray Spectrometer"><em>APXS</em></abbr> for determining
+                                elemental composition of soil, rocks and other materials on the surface of Mars.
+                            </li>
+                            <li class="py-1">
+                                <em>Rover Control Software</em> for coordinating movement, studying terrain features,
+                                and viewing the nearby area in 3D space.
+                            </li>
+                            <li class="py-1">
+                                <em>Three cameras</em>, one color, two monochrome. However, the operators primarily
+                                used images from the lander's IMP camera system, as it had higher quality on top of
+                                having a height advantage over the rover.
+                            </li>
+                        </ul>
+                    </b-col>
+                    <b-col cols="12" lg="5" align-h="top" class="w-100 h-100">
+                        <h3>Autonomy</h3>
+                        <p class="ml-4 ml-sm-3">
+                            Sojourner was an important step in Robotics due to the nature of the mission.
+                            With such a long distance between Mars and Earth, radio communications took around 14
+                            minutes to receive, and then another 14 minutes to send back.
+                        </p>
+                        <p class="ml-4 ml-sm-3">
+                            As such, human telegraphed movements too slow for the mission, and while complete autopilot
+                            is impossible for such a complex mission, the rovers, landers, and spacecraft made by NASA
+                            had to be programmer with autopilot.
+                        </p>
+                        <p class="ml-4 ml-sm-3">
+                            To solve this problem, Sojourner, along with every rover to land on Mars, is equipped with
+                            software dedicated to autonomously moving the rover across the martian surface.
+                        </p>
+                        <p class="ml-4 ml-sm-3">
+                            While Sojourner was comparatively basic in it's software's autonomy, the software was
+                            helpful to operators in visualizing and telegraphing instructions across the rocky terrain
+                            of Ares Vallis.
+                        </p>
+                    </b-col>
+                </b-row>
+            </b-jumbotron>
+            <b-jumbotron fluid container-fluid="fluid" class="text-dark py-3 px-md-5 px-sm-3">
+                <b-row class="px-sm-3 px-md-3 w-100 justify-content-center justify-content-lg-between">
+                    <b-col class="d-block d-lg-none py-2 pb-3 py-lg-0" cols="0" sm="7" lg="0" align-self="center">
+                        <b-img fluid-grow class="shadow-lg" src="./assets/spirit.jpg"></b-img>
+                    </b-col>
+                    <b-col class="d-none d-lg-block py-0 py-lg-4 py-xl-4" cols="0" lg="4" xl="4" align-self="center">
+                        <b-img fluid-grow class="shadow-lg" src="./assets/spirit.jpg"></b-img>
+                    </b-col>
+                    <b-col cols="12" lg="8" xl="7" align-self="center">
+                        <h1 id="spirit">
+                            Spirit
+                        </h1>
+                        <p class="ml-4">
+                            The geology-centric rover was launched into space <abbr
+                            title="Spirit and Opportunity were launched separately, 3 days apart.">with it's
+                            twin</abbr>, Opportunity, on
+                            <em class="text-nowrap" :title="this.moments.spirit.launch.fromNow()">
+                                {{ this.moments.spirit.launch.format("MMMM Do, YYYY") }}</em>.
+                            And on
+                            <em :title="this.moments.sojourner.landing.fromNow()">
+                                {{ this.moments.spirit.landing.format("MMMM Do, YYYY") }}</em>,
+                            it became the 2nd rover to arrive on the red planet.
+                        </p>
+                        <p class="ml-4">
+                            Spirit was planned with just a 90 day mission in mind, but like it's twin, Spirit outlasted
+                            this cycle far beyond what was originally planned.
+                        </p>
                     </b-col>
                 </b-row>
             </b-jumbotron>
@@ -76,7 +166,7 @@
                                 and viewing the nearby area in 3D space.
                             </li>
                             <li class="py-1">
-                                <em>Three cameras</em>, one color, two monochrome. However, the operators primiarily
+                                <em>Three cameras</em>, one color, two monochrome. However, the operators primarily
                                 used images from the lander's IMP camera system, as it had higher quality on top of
                                 having a height advantage over the rover.
                             </li>
@@ -99,92 +189,6 @@
                             software dedicated to autonomously moving the rover across the martian surface.
                         </p>
                         <p class="ml-4 ml-sm-3">
-                            While Sojourner was comparatively basic in it's software's autonomy, the software was
-                            helpful to operators in visualizing and telegraphing instructions across the rocky terrain
-                            of Ares Vallis.
-                        </p>
-                    </b-col>
-                </b-row>
-            </b-jumbotron>
-            <b-jumbotron fluid container-fluid class="text-dark my-0 py-md-5 py-sm-0 px-md-5 px-sm-3">
-                <b-row class="w-100 px-3 px-sm-0" align-h="around">
-                    <b-col cols="10" lg="4" align-self="center">
-                        <b-img fluid-grow class="shadow-lg" src="./assets/spirit.jpg"></b-img>
-                    </b-col>
-                    <b-col class="ml-n5 pl-lg-5 pt-3 pt-lg-0" cols="12" lg="7" align-self="center">
-                        <h1 id="spirit">
-                            Spirit
-                        </h1>
-                        <p class="ml-4">
-                            The geology-centric rover was launched into space <abbr
-                            title="Spirit and Opportunity were launched separately, 3 days apart.">with it's
-                            twin</abbr>, Opportunity, on
-                            <em class="text-nowrap" :title="this.moments.spirit.launch.fromNow()">
-                                {{ this.moments.spirit.launch.format("MMMM Do, YYYY") }}</em>.
-                            And on
-                            <em :title="this.moments.sojourner.landing.fromNow()">
-                                {{ this.moments.spirit.landing.format("MMMM Do, YYYY") }}</em>,
-                            it became the 2nd rover to arrive on the red planet.
-                        </p>
-                        <p class="ml-4">
-                            Spirit was planned with just a 90 day mission in mind, but like it's twin, Spirit outlasted
-                            this cycle far beyond what was originally planned.
-                        </p>
-                    </b-col>
-                </b-row>
-            </b-jumbotron>
-            <b-jumbotron fluid container-fluid="fluid px-0" class="bg-black my-0 py-md-5 py-0 px-3 px-md-1 px-xl-2">
-                <b-row class="px-sm-3 px-md-3 w-100" align-h="around">
-                    <b-col cols="8" lg="7" align-self="top">
-                        <h3>Communications</h3>
-                        <p class="ml-4">
-                            Sojourner is unique among the 4 rovers to successfully deploy on Mars in that it required
-                            it's lander to stay in communication with Earth. The rover was not equipped with a high gain
-                            antenna, which is required in order to communicate with Earth over large distances.
-                        </p>
-                        <p class="ml-4">
-                            This limitation forced the rover to stay within a certain range of the lander, and
-                            throughout it's lifetime on Mars, it never traveled more than a few dozen feet from the
-                            lander.
-                        </p>
-                        <h3>Tools</h3>
-                        <p class="ml-4 mb-1">
-                            Sojourner was designed to study the surface of Mars by collecting and analyzing rock
-                            samples, taking pictures and more using the following tools:
-                        </p>
-                        <ul>
-                            <li class="py-1 pt-2">
-                                An <abbr title="Alpha Proton X-ray Spectrometer"><em>APXS</em></abbr> for determining
-                                elemental composition of soil, rocks and other materials on the surface of Mars.
-                            </li>
-                            <li class="py-1">
-                                <em>Rover Control Software</em> for coordinating movement, studying terrain features,
-                                and viewing the nearby area in 3D space.
-                            </li>
-                            <li class="py-1">
-                                <em>Three cameras</em>, one color, two monochrome. However, the operators primiarily
-                                used images from the lander's IMP camera system, as it had higher quality on top of
-                                having a height advantage over the rover.
-                            </li>
-                        </ul>
-                    </b-col>
-                    <b-col md="6" lg="5" align-h="top" class="w-100 h-100">
-                        <h3>Autonomy</h3>
-                        <p class="ml-3 ml-lg-4">
-                            Sojourner was an important step in Robotics due to the nature of the mission.
-                            With such a long distance between Mars and Earth, radio communications took around 14
-                            minutes to receive, and then another 14 minutes to send back.
-                        </p>
-                        <p class="ml-3 ml-lg-4">
-                            As such, human telegraphed movements too slow for the mission, and while complete autopilot
-                            is impossible for such a complex mission, the rovers, landers, and spacecraft made by NASA
-                            had to be programmer with autopilot.
-                        </p>
-                        <p class="ml-3 ml-lg-4">
-                            To solve this problem, Sojourner, along with every rover to land on Mars, is equipped with
-                            software dedicated to autonomously moving the rover across the martian surface.
-                        </p>
-                        <p class="ml-3 ml-lg-4">
                             While Sojourner was comparatively basic in it's software's autonomy, the software was
                             helpful to operators in visualizing and telegraphing instructions across the rocky terrain
                             of Ares Vallis.
